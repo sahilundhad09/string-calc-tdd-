b89eab09709b882ca943b338c381a04b679e334d
sahilundhad09 <sahilundhad304@gmail.com>
2025-07-03 21:28:17 +0530

    RED: Add test for two numbers

--- test/StringCalculator.test.js
+++ test/StringCalculator.test.js
@@ -10,4 +10,10 @@ describe("StringCalculator", () => {
     const calculator = new StringCalculator()
     expect(calculator.add("1")).toBe(1)
   })
+
+  test("should return sum of two comma-separated numbers", () => {
+    const calculator = new StringCalculator()
+    expect(calculator.add("1,5")).toBe(6)
+    expect(calculator.add("2,3")).toBe(5)
+  })
 })
