59b2531f734fc45b14a88c471c6b8eaf3983535e
sahilundhad09 <sahilundhad304@gmail.com>
2025-07-03 22:39:21 +0530

    GREEN: Ignore numbers bigger than 1000

--- src/StringCalculator.js
+++ src/StringCalculator.js
@@ -8,27 +8,23 @@ class StringCalculator {
     const numberArray = numbersString.split(delimiter)
     const parsedNumbers = numberArray.map((num) => Number.parseInt(num))
 
-    this._checkForNegatives(parsedNumbers)
+    const negatives = parsedNumbers.filter((num) => num < 0)
+    if (negatives.length > 0) {
+      throw new Error(`negative numbers not allowed ${negatives.join(",")}`)
+    }
 
-    return parsedNumbers.reduce((sum, num) => sum + num, 0)
+    return parsedNumbers.filter((num) => num <= 1000).reduce((sum, num) => sum + num, 0)
   }
 
   _parseDelimiter(numbers) {
     if (numbers.startsWith("//")) {
       const delimiterEndIndex = numbers.indexOf("\n")
-      const delimiter = numbers.substring(2, delimiterEndIndex)
+      const customDelimiter = numbers.substring(2, delimiterEndIndex)
       const numbersString = numbers.substring(delimiterEndIndex + 1)
-      return { delimiter, numbersString }
+      return { delimiter: new RegExp(customDelimiter), numbersString }
     }
 
     return { delimiter: /[,\n]/, numbersString: numbers }
-  }
-
-  _checkForNegatives(numbers) {
-    const negatives = numbers.filter((num) => num < 0)
-    if (negatives.length > 0) {
-      throw new Error(`negative numbers not allowed ${negatives.join(",")}`)
-    }
   }
 }
 
