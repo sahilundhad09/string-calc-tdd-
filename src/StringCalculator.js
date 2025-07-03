83800b960296cba22ed47931b0bb9bb02da70493
sahilundhad09 <sahilundhad304@gmail.com>
2025-07-03 22:13:53 +0530

    REFACTOR: Extract delimiter parsing logic

--- src/StringCalculator.js
+++ src/StringCalculator.js
@@ -3,20 +3,22 @@ class StringCalculator {
     if (numbers === "") {
       return 0
     }
-    
-   let delimiter = /[,\n]/
-    let numbersString = numbers
 
-    if (numbers.startsWith("//")) {
-      const delimiterEndIndex = numbers.indexOf("\n")
-      delimiter = numbers.substring(2, delimiterEndIndex)
-      numbersString = numbers.substring(delimiterEndIndex + 1)
-    }
-
+    const { delimiter, numbersString } = this._parseDelimiter(numbers)
     const numberArray = numbersString.split(delimiter)
     return numberArray.reduce((sum, num) => sum + Number.parseInt(num), 0)
+  }
 
+  _parseDelimiter(numbers) {
+    if (numbers.startsWith("//")) {
+      const delimiterEndIndex = numbers.indexOf("\n")
+      const delimiter = numbers.substring(2, delimiterEndIndex)
+      const numbersString = numbers.substring(delimiterEndIndex + 1)
+      return { delimiter, numbersString }
+    }
+
+    return { delimiter: /[,\n]/, numbersString: numbers }
   }
 }
 
-module.exports = StringCalculator;
+module.exports = StringCalculator
