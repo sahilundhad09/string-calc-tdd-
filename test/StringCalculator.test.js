72520e8f0aa4dfeb5e736769603575bdf7a77a1a
sahilundhad09 <sahilundhad304@gmail.com>
2025-07-03 22:07:03 +0530

    RED: Add test for custom delimiters

--- test/StringCalculator.test.js
+++ test/StringCalculator.test.js
@@ -28,4 +28,10 @@ describe("StringCalculator", () => {
     expect(calculator.add("1\n2,3")).toBe(6)
     expect(calculator.add("1\n2\n3")).toBe(6)
   })
+
+  test("should support custom delimiters", () => {
+    const calculator = new StringCalculator()
+    expect(calculator.add("//;\n1;2")).toBe(3)
+    expect(calculator.add("//|\n1|2|3")).toBe(6)
+  })
 })
