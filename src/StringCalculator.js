e66f845b56f9efd62effeb239478c0c56f0580f3
sahilundhad09 <sahilundhad304@gmail.com>
2025-07-03 22:04:16 +0530

    GREEN: Support newline delimiters using regex

--- src/StringCalculator.js
+++ src/StringCalculator.js
@@ -3,12 +3,10 @@ class StringCalculator {
     if (numbers === "") {
       return 0
     }
+    
+   const numberArray = numbers.split(/[,\n]/)  
+       return numberArray.reduce((sum, num) => sum + Number.parseInt(num), 0)
 
-    if (!numbers.includes(",")) 
-        return parseInt(numbers);
-
-    const numberArray = numbers.split(",")
-    return numberArray.reduce((sum, num) => sum + Number.parseInt(num), 0)
   }
 }
 
